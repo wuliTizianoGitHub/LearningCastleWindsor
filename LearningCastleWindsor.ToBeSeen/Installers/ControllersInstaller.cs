c8e16fa75f19040752b347e18e4a4638cbcc1c6b
wuliTizianoGitHub <Tiziano.Impossible@gmail.com>
2017-03-22 17:32:16 +0800

    tainj

---
+++ LearningCastleWindsor.ToBeSeen/Installers/ControllersInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System.Web.Mvc;
+
+namespace LearningCastleWindsor.ToBeSeen.Installers
+{
+    public class ControllersInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly().BasedOn<IController>().LifestyleTransient());
+        }
+    }
+}
---
+++ LearningCastleWindsor.ToBeSeen/Tests/ControllersInstallerTests.cs
@@ -0,0 +1,51 @@
+﻿using Castle.MicroKernel;
+using Castle.Windsor;
+using LearningCastleWindsor.ToBeSeen.Installers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xunit;
+
+namespace LearningCastleWindsor.ToBeSeen.Tests
+{
+    public class ControllersInstallerTests
+    {
+        private IWindsorContainer containerWithControllers;
+
+        public ControllersInstallerTests()
+        {
+            containerWithControllers = new WindsorContainer().Install(new ControllersInstaller());
+        }
+
+        [Fact]
+        public void All_controllers_implement_IController()
+        {
+            var allHandlers = GetAllHandlers(containerWithControllers);
+
+            var controllerHandlers = GetHandlersFor(typeof(IController), containerWithControllers);
+
+            Assert.NotEmpty(allHandlers);
+
+            Assert.Equal(allHandlers, controllerHandlers);
+        }
+
+        private IHandler[] GetAllHandlers(IWindsorContainer container)
+        {
+            return GetHandlersFor(typeof(object),container);
+        }
+
+        private IHandler[] GetHandlersFor(Type type, IWindsorContainer container)
+        {
+            return container.Kernel.GetAssignableHandlers(type);
+        }
+
+        [Fact]
+        public void All_Controllers_are_registered()
+        {
+
+        }
+    }
+}
